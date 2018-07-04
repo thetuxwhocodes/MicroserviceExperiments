7841387013faa864f47e3da958b957415fd20649
Girish B <tuxinvincible@gmail.com>
2018-07-04 22:36:23 +0530

    Initial changes

---
+++ src/main/java/com/girish/microservices/toolset/ToolsetApplication.java
@@ -0,0 +1,12 @@
+package com.girish.microservices.toolset;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolsetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToolsetApplication.class, args);
+	}
+}
