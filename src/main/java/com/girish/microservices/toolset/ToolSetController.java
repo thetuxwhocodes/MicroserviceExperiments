85f732d03deaca260bc2a106db61b775eca28ad0
Girish B <tuxinvincible@gmail.com>
2018-07-04 22:46:48 +0530

    Added the Tools list initial version. Upgraded to Java 10.

--- src/main/java/com/girish/microservices/toolset/ToolSetController.java
+++ src/main/java/com/girish/microservices/toolset/ToolSetController.java
@@ -1,5 +1,7 @@
 package com.girish.microservices.toolset;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,5 +12,12 @@ public class ToolSetController {
 	public String sayHello() {
 		
 		return "Welcome to the world of Tools!";
+	}
+	
+	@GetMapping("/tools")
+	public List<String> getTools() {
+		
+		List<String> tools = List.of("Calc", "Temp Conv");
+		return tools;
 	}
 }
